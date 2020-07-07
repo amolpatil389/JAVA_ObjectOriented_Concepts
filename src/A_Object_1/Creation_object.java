6c4c57fe4fd51cedaaebb872f37c9c5d20c3f964
Patil <amol.patil@here.com>
2020-07-07 23:39:17 +0530

    Created 1st Object Program

---
+++ src/A_Object_1/Creation_object.java
@@ -0,0 +1,27 @@
+//Object is just an intance of class
+
+
+package A_Object_1;
+
+public class Creation_object {
+	
+	String name;
+	int id;
+
+	public static void main(String[] args) {
+	
+		
+		Creation_object obj1 = new Creation_object();
+		
+		obj1.id=1;
+		obj1.name = "Programing";
+		
+		System.out.println(obj1.id);
+		
+		System.out.println(obj1.name);
+		
+
+	}
+
+}
+
