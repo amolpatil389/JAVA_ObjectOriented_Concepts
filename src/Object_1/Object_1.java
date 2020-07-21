86d198585af1ab4b932da81a5a8ab2d04668566c
Patil <amol.patil@here.com>
2020-07-21 12:44:35 +0530

    Deleted and again copied

---
+++ src/Object_1/Object_1.java
@@ -0,0 +1,25 @@
+package Object_1;
+
+public class Object_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Emp e1 = new Emp();
+//		e1.display_nmae();
+
+		String s = "amol";
+		System.out.println(s);
+		
+		String result = s.toUpperCase();
+		System.out.println(result);
+
+	}
+
+}
+//
+//class Emp {
+//	public void display_nmae() {
+//		System.out.println("Hello world");
+//	}
+//}
